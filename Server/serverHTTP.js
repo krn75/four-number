e6874206d95dd7edde70f6afd8f71f90eee05beb
Ruslan Kozubov <krn75@mail.ru>
2017-01-22 01:02:37 +0200

    start

---
+++ Server/serverHTTP.js
@@ -0,0 +1,43 @@
+var http = require("http");
+var headers = require("./headers");
+var express = require("express"); //npm install express
+var app = express();
+
+http.createServer(app).listen(8081);
+console.log("Server started  on port 80");
+
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+
+
+//Access
+app.use("/*",function (req,res,next) {
+    headers.setHeaders(res);
+    next();
+});
+
+//Router
+app.get("/",function (req,res) {
+    res.end(JSON.stringify(getCoords()));
+});
+
+function getCoords() {
+    var x0=100;
+    var y0=100;
+
+    var d=10;
+    var res = {};
+
+    res.x=x0+rand(d);
+    res.y=y0+rand(d);
+
+    return res;
+};
+
+function rand(x) {
+    return x * (2*Math.random()-1);
+};
+
+
