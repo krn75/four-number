5b4ab52743ccb10ade9c9485dfe86644548fe99e
Ruslan Kozubov <krn75@mail.ru>
2017-01-22 17:26:55 +0200

    test

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-// var port = process.env.PORT || 8080;
-//
-//
-// // set the home page route
-// app.get('/', function(req, res) {
-//     res.end('index');
-// });
-//
-// app.listen(port, function() {
-//     console.log('Our app is running on http://localhost:' + port);
+// app.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 // });
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// set the port of our application
+//process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
+
+
+// set the home page route
+app.get('/', function(req, res) {
+    res.end('index');
+});
+
